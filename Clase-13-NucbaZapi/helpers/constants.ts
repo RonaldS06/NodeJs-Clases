e9608c4550985a8682c905df6686e1b6b71cdbd8
Ronald Santamaria <pizarroronald06@gmail.com>
2024-07-20 19:20:05 -0300

    Final Nodejs

--- Clase-13-NucbaZapi/helpers/constants.ts
+++
@@ -1,4 +0,0 @@
-export const ROLES = {
-  admin: "23yun4dm1n",
-  user: "AstaJ3J3",
-};
---
+++ Clase13-NucbaZappi/Nucbazappi-Front/src/utils/constants.js
@@ -0,0 +1,4 @@
+// export const BASE_URL = "https://nucba-zapi-dun.vercel.app:8080";
+export const BASE_URL = "https://nodejs-back-ronalds06.vercel.app:8080";
+export const ADMIN = '50yun4dm1n';
+export const USER = '50yunu53r';
